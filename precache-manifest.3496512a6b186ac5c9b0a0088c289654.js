e439e13a80036c1f31de0fa6c4a307b89f4ee28c
Jimmy Börjesson <jimmy@boerjesson.se>
2020-07-14 23:48:06 +0200

    Updates

---
+++ precache-manifest.3496512a6b186ac5c9b0a0088c289654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d433773212c6ca5b58b034d0b2fb2a3c",
+    "url": "/image-defrag/index.html"
+  },
+  {
+    "revision": "ef12b2a20ccdca9c1ea2",
+    "url": "/image-defrag/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edc18fc836a8bfe85ff5",
+    "url": "/image-defrag/static/js/2.f13bf6e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-defrag/static/js/2.f13bf6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef12b2a20ccdca9c1ea2",
+    "url": "/image-defrag/static/js/main.e1672ca7.chunk.js"
+  },
+  {
+    "revision": "6f8b9c40a7b464190d0b",
+    "url": "/image-defrag/static/js/runtime-main.47adac8b.js"
+  }
+]);
